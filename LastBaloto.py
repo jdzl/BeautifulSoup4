905fb8b6124a669688ca661ed96a06c4b1c455b5
David Zambrano <ggmortum@hotmail.com>
2016-07-25 18:32:23 -0500

    ---

---
+++ LastBaloto.py
@@ -0,0 +1,16 @@
+from bs4    import BeautifulSoup
+from urllib import urlopen
+
+url = 'http://www.baloto.com/#node4de5.html?qt-navegacion_internas_baloto=2#qt-navegacion_internas_baloto'
+web = urlopen(url).read()
+html = BeautifulSoup(web,"html.parser")
+
+
+div= html.findAll('div',
+                  attrs={'class': "content"}
+                  )
+
+span= div[0].findAll('span')
+print "Ultimo juego Baloto"
+for i in range(4,10):    
+    print span[i].text,
